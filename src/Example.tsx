0e0c37dd1aa34c299c05cd2cbbe404e646192325
Karlkim Suwamongkol <contact@karlk.im>
2019-01-31 21:42:56 -0600

    useEffect

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,7 +1,22 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 function Example() {
     const [count, setCount] = useState(0)
+    const [init, setInit] = useState(false)
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `${count}`
+    })
+
+    useEffect(() => {
+        alert(count)
+    }, [init])
+
+    useEffect(() => {
+        setInit(true)
+    }, [init])
 
     return (
         <div>
