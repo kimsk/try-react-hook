c02e63c11dd187ad8fac85784dadc175d981c65d
Karlkim Suwamongkol <contact@karlk.im>
2019-02-06 21:55:42 -0600

    pull hooks to another ts file

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,48 +1,6 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 
-function useValue (initValue: string) {
-    const [value, setValue] = useState(initValue)
-
-    function onChange (e: any) {
-        setValue(e.target.value)
-    }
-
-    return { value, onChange }
-}
-
-function useDocumentTitle (count: number) {
-    console.log('useDocumentTitle')
-    // Similar to componentDidMount and componentDidUpdate:
-    useEffect(() => {
-        console.log('useDocumentTitle: useEffect')
-
-        // Update the document title using the browser API
-        document.title = `${count}`
-
-        return () => {
-            console.log('useDocumentTitle: useEffect: Return')
-        }
-    })
-    
-    console.log('useAlert: exit')
-}
-
-function useAlert (count: number) {
-    console.log('useAlert: enter')
-    const [init, setInit] = useState(false)
-
-    useEffect(() => {
-        console.log('useAlert: useEffect: setInit')
-        setInit(true)
-    }, [init])
-
-    useEffect(() => {
-        console.log('useAlert: useEffect: alert')
-        alert(count)
-    }, [init])
-
-    console.log('useAlert: exit')
-}
+import { useAlert, useDocumentTitle, useValue } from "./hooks"
 
 function Example() {
     console.log('Example')
